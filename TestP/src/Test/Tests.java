b96ef0babeeeb677297c6c6368c3910e84170739
ANKIT NIGAM <ANKIT NIGAM@192.168.0.8>
2019-02-08 00:49:16 +0530

    Version 0.0.2

--- TestP/src/Test/Tests.java
+++ TestP/src/Test/Tests.java
@@ -6,14 +6,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Tests {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hi");
+		
+		
 		String key="webdriver.chrome.driver";
 		String value="./exe/chromedriver.exe";
 		System.setProperty(key, value);
-		WebDriver driver= new ChromeDriver();
+		WebDriver driver= new ChromeDriver();//upcasting..
 		driver.get("https://www.google.com");
-		System.out.println("run successfully...");
+		System.out.println("run successfully");
 	}
 
 }
